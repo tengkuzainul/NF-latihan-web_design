92f033ee22dd7bc05dfff07e26e2036515516395
tengkuzainul <tengkuzainula04@gmail.com>
2024-03-19 10:40:20 +0700

    DOM JS TRANSAKSI SWAL

--- js/latihanJS14.js
+++ js/latihanJS14.js
@@ -12,4 +12,43 @@ function transaksi() {
   const nama = form.nama.value;
   const produk = form.produk.value;
   const jumlah = form.jumlah.value;
+
+  let harga;
+
+  switch (produk) {
+    case "TV":
+      harga = 20000000;
+      break;
+    case "AC":
+      harga = 40000000;
+      break;
+    case "Kulkas":
+      harga = 100000000;
+      break;
+    default:
+      harga = 0;
+      break;
+  }
+
+  let hargaKotor = harga * jumlah;
+
+  let diskon;
+  if (produk === "Kulkas" && jumlah >= 3) {
+    diskon = 0.3 * hargaKotor;
+  } else if (produk === "AC" && jumlah >= 3) {
+    diskon = 0.2 * hargaKotor;
+  } else {
+    diskon = 0.1 * hargaKotor;
+  }
+
+  swal({
+    title: "Good job!",
+    text: `Nama Pelanggan: ${nama}
+           Produk Yang Dibeli: ${produk}
+           Harga Produk: ${hargaKotor}
+           Qty: ${jumlah}
+           Harga Total : ${diskon}`,
+    icon: "success",
+    button: "Done!",
+  });
 }
